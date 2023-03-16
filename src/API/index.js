aac79f250af68a969c142d485f0a5c2ece6b125e
Ubedullah Mohammed <ubedullah.mohammed@tawfeeqtnt.com>
2023-03-16 12:20:26 +0300

    Deploy Webiste

--- src/API/index.js
+++ src/API/index.js
@@ -1,17 +1,18 @@
-export const getAllProducts = () => {
-  return fetch("https://dummyjson.com/products").then((res) => res.json());
+export const getAllProducts = async () => {
+  const res = await fetch("https://dummyjson.com/products");
+  return await res.json();
 };
-export const getProductsByCategory = (category) => {
-  return fetch(`https://dummyjson.com/products/category/${category}`).then(
-    (res) => res.json()
-  );
+export const getProductsByCategory = async (category) => {
+  const res = await fetch(`https://dummyjson.com/products/category/${category}`);
+  return await res.json();
 };
-export const getCart = () => {
-  return fetch("https://dummyjson.com/carts/1").then((res) => res.json());
+export const getCart = async () => {
+  const res = await fetch("https://dummyjson.com/carts/1");
+  return await res.json();
 };
 
-export const addToCart = (id) => {
-  return fetch("https://dummyjson.com/carts/add", {
+export const addToCart = async (id) => {
+  const res = await fetch("https://dummyjson.com/carts/add", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
@@ -23,5 +24,6 @@ export const addToCart = (id) => {
         },
       ],
     }),
-  }).then((res) => res.json());
+  });
+  return await res.json();
 };
