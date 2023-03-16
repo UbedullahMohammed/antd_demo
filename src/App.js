b2aee6d14626ff9c2f0ee372a8762be6b0250cd5
Ubedullah Mohammed <ubedullah.mohammed@tawfeeqtnt.com>
2023-03-16 12:03:52 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import AppFooter from './components/Footer';
+import AppHeader from './components/Header';
+import PageContent from './components/PageContent';
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <AppHeader />
+        <PageContent />
+        <AppFooter />
+      </BrowserRouter>
+    </div>    
+    );
+}
+
+export default App;
